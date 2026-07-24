5188428181765ec19b4db4c584d1bead43029ea8
cuiweixuan <linghua0214un@gmail.com>
2026-07-24 17:11:53 +0800

    全局唯一ID业务代码开发

---
+++ src/main/java/com/hmdp/utils/RedisIdWorker.java
@@ -0,0 +1,47 @@
+package com.hmdp.utils;
+
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import javax.servlet.http.PushBuilder;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RedisIdWorker {
+
+    public static final long BEGIN_TIMESTAMP = 1767225600L;
+    //序列号位数
+    public static final int COUNT_BITS = 32;
+
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
+
+    public long nextId(String keyPrefix){
+        //前缀参数区分不同的业务
+        //1.生成时间戳
+        LocalDateTime now = LocalDateTime.now();
+        long nowSecond = now.toEpochSecond(ZoneOffset.UTC);
+        long timestamp = nowSecond - BEGIN_TIMESTAMP;
+
+        //2.生成序列号
+        //获取当前日期
+        String date = now.format(DateTimeFormatter.ofPattern("yyyy:MM:dd"));
+        //自增长
+        long count = stringRedisTemplate.opsForValue().increment("icr:" + keyPrefix + ":" + date);
+
+        //3.拼接并返回
+        return timestamp << COUNT_BITS | count;
+    }
+
+    public static void main(String[] args) {
+        LocalDateTime time = LocalDateTime.of(2026, 1, 1, 0, 0, 0);
+        long second = time.toEpochSecond(ZoneOffset.UTC);
+        System.out.println(second);
+    }
+
+}
